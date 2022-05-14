814d4ba93df655c8113a1085829aaaec67f2afff
JessieGeorge <emailjes@gmail.com>
2022-05-13 21:48:10 -0700

    changed bit logic to math logic

--- src/GenericUCQ.java
+++ src/GenericUCQ.java
@@ -1,8 +1,5 @@
 public class GenericUCQ {
 	
-	// 256 color values, 3 because we store R,G,B for each color
-	static int[][] LUT = new int[256][3];
-	
 	// number of index bits for a channel, named as per Canvas
 	int nr, ng, nb;
 	
@@ -11,8 +8,13 @@ public class GenericUCQ {
 	
 	int totalBits;
 	
+	int numColors;
+	
+	// look up table
+	int[][] LUT;
+	
 	// quantization steps for a channel
-	int quantr, quantg, quantb;
+	double quantr, quantg, quantb;
 	
 	// Floyd weight factors for error diffusion
 	final double rightFloyd = 7/16.0;
@@ -22,45 +24,44 @@ public class GenericUCQ {
 	
 	String originalImageName;
 	
+	final boolean testPrint = false;
+	
 	public GenericUCQ(int nr, int ng, int nb) {
 		this.nr = nr;
 		this.ng = ng;
 		this.nb = nb; 
-		channelBits = "r" + nr + "g" + ng + "b" + nb;
+		this.channelBits = "r" + nr + "g" + ng + "b" + nb;
 		this.totalBits = nr + ng + nb;
 		
-		this.quantr = (int)Math.pow(2, 8 - nr);
-		this.quantg = (int)Math.pow(2, 8 - ng);
-		this.quantb = (int)Math.pow(2, 8 - nb);
+		//this.numColors = (int)Math.pow(2, totalBits); // TODO: uncomment?
+		this.numColors = 256;
+		
+		// 3 because we store R,G,B values for each color
+		this.LUT = new int[numColors][3];
+		
+		if (testPrint) {
+			System.out.println("totalBits = " + totalBits); // REMOVETHIS
+			System.out.println("numColors = " + numColors); // REMOVETHIS
+		}
+		
+		this.quantr = Math.pow(2, 8 - nr);
+		this.quantg = Math.pow(2, 8 - ng);
+		this.quantb = Math.pow(2, 8 - nb);
 	}
 	
 	public void initLUT() {
+		
 		System.out.println();
 		System.out.println("LUT by UCQ");
 		System.out.println("Index	R	G	B");
 		System.out.println("-------------------------------------------------");
 		
-		for (int i = 0; i <= 255; i++) {
-			String binaryIndex = Integer.toBinaryString(i);
-			// making binaryIndex have totalBits number of bits
-			String formatter = "%" + totalBits + "s";
-			String binaryIndexPadded = String.format(formatter, binaryIndex)
-					.replaceAll(" ", "0");
-			
-			// BI stands for binary index
-			String redBI = binaryIndexPadded.substring(0, nr);
-			String greenBI = binaryIndexPadded.substring(nr, nr + ng);
-			String blueBI = binaryIndexPadded.substring(nr + ng, totalBits);
-			
-			// parse binary string to decimal integer
-			int redInt = Integer.parseInt(redBI, 2);
-			int greenInt = Integer.parseInt(greenBI, 2);
-			int blueInt = Integer.parseInt(blueBI, 2);
+		for (int i = 0; i < numColors; i++) {
 			
 			// representative color in the center of the range
-			int red = redInt * quantr + (quantr / 2);
-			int green = greenInt * quantg + (quantg / 2);
-			int blue = blueInt * quantb + (quantb / 2);
+			int red = (int)(Math.floor(i/quantr) * quantr + (quantr / 2));
+			int green = (int)(Math.floor(i/quantg) * quantg + (quantg / 2));
+			int blue = (int)(Math.floor(i/quantb) * quantb + (quantb / 2));
 			
 			LUT[i][0] = red;
 			LUT[i][1] = green;
@@ -69,6 +70,11 @@ public class GenericUCQ {
 			System.out.println(i + "\t" + red + "\t" + green + "\t" + blue);
 		}
 		System.out.println();
+		
+		/*
+		System.out.println("I'M EXITING!"); // REMOVETHIS
+		System.exit(1); // REMOVETHIS
+		*/
 	}
 	
 	public boolean isValidPos(MImage img, int x, int y) {
@@ -148,15 +154,15 @@ public class GenericUCQ {
 		}
 	}
 	
-	public MImage createIndexImage(MImage img, String imageShortName) {
+	public void createQuantizedImage(String imageShortName) {
 		
 		System.out.println("\nMaking a copy of original image to edit"
 				+ " for error diffusion.");
 		MImage copyImg = new MImage(originalImageName);
 		
 		System.out.println("\nMaking a copy of original image to edit"
-				+ " for grayscale index image.");
-		MImage indexImg = new MImage(originalImageName);
+				+ " for quantized image.");
+		MImage quantizedImg = new MImage(originalImageName);
 		
 		int w = copyImg.getW();
 		int h = copyImg.getH();
@@ -165,6 +171,8 @@ public class GenericUCQ {
 		int originalRed, originalGreen, originalBlue;
 		int quantizedRed, quantizedGreen, quantizedBlue;
 		int errorRed, errorGreen, errorBlue;
+		
+		int[] quantizedRGB = new int[3];
 		
 		for (int x = 0; x < w; x++) {
 			for (int y = 0; y < h; y++) {
@@ -175,34 +183,15 @@ public class GenericUCQ {
 				originalGreen = rgb[1];
 				originalBlue = rgb[2];
 				
-				int redIndex = (int)Math.floor(rgb[0] / quantr);
-				int greenIndex = (int)Math.floor(rgb[1] / quantg);
-				int blueIndex = (int)Math.floor(rgb[2] / quantb);
+				if (testPrint) {
+					System.out.println("originalRed = " + originalRed); /// REMOVETHIS
+					System.out.println("originalGreen = " + originalGreen); /// REMOVETHIS
+					System.out.println("originalBlue = " + originalBlue); /// REMOVETHIS
+				}
 				
-				// BI stands for binary index
-				String redBI = Integer.toBinaryString(redIndex);
-				String greenBI = Integer.toBinaryString(greenIndex);
-				String blueBI = Integer.toBinaryString(blueIndex);
-				
-				// format color channels based on specified number of index bits
-				String redformatter = "%" + nr + "s";
-				String greenformatter = "%" + ng + "s";
-				String blueformatter = "%" + nb + "s";
-				String redBIPadded = String.format(redformatter, redBI)
-						.replaceAll(" ", "0");
-				String greenBIPadded = String.format(greenformatter, greenBI)
-						.replaceAll(" ", "0");
-				String blueBIPadded = String.format(blueformatter, blueBI)
-						.replaceAll(" ", "0");
-				
-				// look up table binary index
-				String lutBI = redBIPadded + greenBIPadded + blueBIPadded;
-				
-				int lutIndex = Integer.parseInt(lutBI, 2);
-				
-				quantizedRed = LUT[lutIndex][0];
-				quantizedGreen = LUT[lutIndex][1];
-				quantizedBlue = LUT[lutIndex][2];
+				quantizedRed = LUT[originalRed][0];
+				quantizedGreen = LUT[originalGreen][1];
+				quantizedBlue = LUT[originalBlue][2];
 				
 				errorRed = originalRed - quantizedRed;
 				errorGreen = originalGreen - quantizedGreen;
@@ -210,48 +199,15 @@ public class GenericUCQ {
 				
 				errorDiffuse(copyImg, x, y, errorRed, errorGreen, errorBlue);
 				
-				// make gray-scale
-				rgb[0] = lutIndex;
-				rgb[1] = lutIndex;
-				rgb[2] = lutIndex;
-				
-				indexImg.setPixel(x, y, rgb);
-			}
-		}
-		
-		// Save it into another PPM file.
-		indexImg.write2PPM(imageShortName + "-index-" + channelBits + ".ppm");
-		
-		return indexImg;
-	}
-	
-	public void createQuantizedImage(MImage indexImg, 
-			String imageShortName) {
-		
-		int w = indexImg.getW();
-		int h = indexImg.getH();
-		int[] rgb = new int[3];
-		
-		System.out.println("\nMaking a copy of original image to edit"
-				+ " for quantized image.");
-		MImage quantizedImg = new MImage(originalImageName);
-		
-		for (int x = 0; x < w; x++) {
-			for (int y = 0; y < h; y++) {
-				indexImg.getPixel(x, y, rgb);
-				int gray = rgb[0];
-				
-				rgb[0] = LUT[gray][0];
-				rgb[1] = LUT[gray][1];
-				rgb[2] = LUT[gray][2];
-				
-				quantizedImg.setPixel(x, y, rgb);
+				quantizedRGB[0] = quantizedRed;
+				quantizedRGB[1] = quantizedGreen;
+				quantizedRGB[2] = quantizedBlue;
+				quantizedImg.setPixel(x, y, quantizedRGB);
 			}
 		}
 		
 		// Save it into another PPM file.
 		quantizedImg.write2PPM(imageShortName + "-QT-" + channelBits + ".ppm");
-		
 	}
 	
 	public void process(MImage img, String imageShortName) {
@@ -259,10 +215,8 @@ public class GenericUCQ {
 		originalImageName = img.getName();
 		
 		initLUT();
-		
-		MImage indexImg = createIndexImage(img, imageShortName);
 
-		createQuantizedImage(indexImg, imageShortName);
+		createQuantizedImage(imageShortName);
 	}
 
 }
